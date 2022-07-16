7fecaa23ebdf9d44cc9173b342be137e7d5bc2a4
Mikołaj Wasiak <m.wasiak11@student.uw.edu.pl>
2022-07-16 13:50:52 +0200

    roll-button

--- scenes/camera.gd
+++ scenes/camera.gd
@@ -1,6 +1,9 @@
 extends Camera
 
 export (float) var speed = -0.001
+export (NodePath) var np_dice
+
+onready var dice = get_node(np_dice)
 
 func _ready():
 	pass # Replace with function body.
@@ -8,3 +11,9 @@ func _ready():
 func _unhandled_input(event):
 	if event is InputEventMouseMotion and Input.is_action_pressed("mouse_right"):
 		transform = transform.translated(Vector3(event.relative.x, 0, event.relative.y) * speed)
+
+
+func _on_roll_pressed():
+	if dice and dice.is_class("Dice"):
+		
+		dice.roll(Vector3.UP, 2)
