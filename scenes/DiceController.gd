78b5d874faeb44cf171c29c95e61fc91c0f47fc9
Jx <jayexec@pm.me>
2022-07-17 09:14:13 +0200

    Fixed cube behavior

---
+++ scenes/DiceController.gd
@@ -0,0 +1,55 @@
+extends Spatial
+
+signal rolled
+
+onready var dice = $Dice
+onready var guard = $DiceGuard
+onready var timer = $StopCountdown
+onready var lock_timer = $StartLock
+
+var collider = null
+
+# Amount of time to wait after throw.
+export var start_lock_time = 0.5
+
+# Amount of time from cube stop to signal delivery.
+export var stop_countdown_time = 0.5
+
+var last_pos = translation
+var rolling = false
+var locked = false
+
+func guard_movement():
+	guard.translation = dice.translation
+	collider = guard.get_collider()
+	var pos = guard.translation
+	if collider != null:
+		if pos.distance_squared_to(last_pos) < 1e-6:
+			if timer.is_stopped():
+				timer.start(stop_countdown_time)
+		else:
+			timer.stop()
+	last_pos = pos
+
+func _process(_delta):
+	if (rolling):
+		guard_movement()
+
+func roll(direction):
+	rolling = true
+	locked = true
+	lock_timer.start(start_lock_time)
+	dice.roll(direction)
+
+func _on_StopCountdown_timeout():
+	var colliding = 7 - int(collider.get_name()[5])
+	print("Thrown: " + String(colliding))
+	rolling = false
+	timer.stop()
+	# TODO: Choose correct side and pass it into this function
+	# It needs side node (with 3d sprites)
+	emit_signal("rolled", SideGenerator.side_to_tile(null))
+
+
+func _on_StartLock_timeout():
+	locked = false
--- scenes/camera.gd
+++ scenes/camera.gd
@@ -1,9 +1,9 @@
 extends Camera
 
 export (float) var speed = -0.025
-export (NodePath) var np_dice
+export (NodePath) var np_dice_controller
 
-onready var dice = get_node(np_dice)
+onready var dice_controller = get_node(np_dice_controller)
 
 func _ready():
 	pass # Replace with function body.
@@ -15,11 +15,12 @@ func _unhandled_input(event):
 		transform.origin += transformed
 
 func _on_roll_pressed():
+	var dice = dice_controller.get_node("Dice")
 	if dice and dice.is_class("Dice"):
 		var dice_target_position = viewport_to_map(get_viewport().size * Vector2(0.5, 0.5))
 		
 		if dice_target_position:
-			dice.roll(Vector3.UP + (dice_target_position - dice.transform.origin) / 10)
+			dice_controller.roll(Vector3.UP + (dice_target_position - dice.transform.origin) / 10)
 
 func viewport_to_map(pos: Vector2):
 	var dropPlane  = Plane(Vector3(0, 1, 0), 0)
